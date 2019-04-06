053b641ad9d0555093bb9db1c8f37206e5c5a60d
Trey Arnold <arnold.trey80@gmail.com>
2019-04-05 21:35:08 -0500

    initial structure and linking assets and libraries

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyBKbm-cOthbu3xo0EVUie600BB-94cX-mE",
+  authDomain: "rockpaperscissors-3c9bc.firebaseapp.com",
+  databaseURL: "https://rockpaperscissors-3c9bc.firebaseio.com",
+  projectId: "rockpaperscissors-3c9bc",
+  storageBucket: "rockpaperscissors-3c9bc.appspot.com",
+  messagingSenderId: "245766332116"
+};
+firebase.initializeApp(config);
+
+const DB = firebase.database();
