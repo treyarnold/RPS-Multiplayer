e4eaa61e7eef09b14681e4df3aa472db5dcd1062
Trey Arnold <arnold.trey80@gmail.com>
2019-04-07 19:12:04 -0500

    total DB redo and multi login now working

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,28 +9,40 @@ const config = {
 firebase.initializeApp(config);
 
 const DB = firebase.database();
+const connectionsRef = DB.ref("/players");
+const connectedRef = DB.ref(".info/connected");
+
+connectedRef.on("value", snapshot => {
+  if (snapshot.val()) {
+    const con = connectionsRef.push(true);
+    game.localID = con.key;
+    con.onDisconnect().remove();
+
+  }
+})
 
 const game = {
   localPlayerNumber: undefined,
   wins: 0,
   lossed: 0,
+  localID: undefined,
 
   addPlayerInput: function (playerNumber) {
     let playerID = `#player${playerNumber}`;
     let display =
       `<form class="input-group mb-3">
-        <input type="text" class="form-control text-center" 
-          placeholder="Player ${playerNumber}" id="player${playerNumber}Name">
-        <button class="input-group-append btn btn-outline-success player" 
-          type="submit" id="${playerNumber}">Submit</button>
-      </form>`
+      <input type="text" class="form-control text-center" 
+        placeholder="Player ${playerNumber}" id="player${playerNumber}Name">
+      <button class="input-group-append btn btn-outline-success player" 
+        type="submit" id="${playerNumber}">Submit</button>
+    </form>`
     $(playerID).empty();
-    $(playerID).append(display);    
+    $(playerID).append(display);
   },
 
-  showPlayer: function (player, playerID) {
+  showPlayer: function (playerName, playerID) {
     $(playerID).empty();
-    const newPlayerHeader = $("<h2>").text(player.name);
+    const newPlayerHeader = $("<h2>").text(playerName);
     $(playerID).append(newPlayerHeader);
   },
 };
@@ -39,23 +51,34 @@ $(document).on("click", ".player", event => {
   event.preventDefault();
   const playerNumber = event.target.id;
   const player = {
-    name: $(`#player${playerNumber}Name`).val().trim(),
-    playerNumber: playerNumber,
+    playerName: $(`#player${playerNumber}Name`).val().trim(),
+    playerNumber: `player${playerNumber}`,
+    wins: 0,
+    losses: 0,
   };
   game.localPlayerNumber = playerNumber;
-  DB.ref("players").push(player);
-  if (playerNumber === "1") DB.ref("gameState/players").update({player1: true});
-  if (playerNumber === "2") DB.ref("gameState/players").update({player2: true});
-  game.showPlayer(player, `#player${playerNumber}`);
+  DB.ref(`players/${game.localID}`).update(player);
 });
 
-DB.ref("gameState/players").on("value", snapshot => {
+DB.ref("players").on("value", snapshot => {
   if (snapshot.val() === null) {
     game.addPlayerInput("1");
     game.addPlayerInput("2");
-  } else {
-    if (!(snapshot.val().player1)) game.addPlayerInput("1")
-    else 
-    if (!(snapshot.val().player2)) game.addPlayerInput("2")
+    return true;
   }
+  let player1 = false;
+  let player2 = false;
+  snapshot.forEach(childSnapshot => {
+    if (childSnapshot.val().playerNumber === "player1") {
+      player1 = true;
+      game.showPlayer(childSnapshot.val().playerName, "#player1");
+    }
+    if (childSnapshot.val().playerNumber === "player2") {
+      player2 = true;
+      game.showPlayer(childSnapshot.val().playerName, "#player2");
+    }
+  });
+if (!player1) game.addPlayerInput("1")
+if (!player2) game.addPlayerInput("2")  
 });
+
--- psuedo.md
+++ psuedo.md
@@ -3,9 +3,9 @@
 ## Outline
 
 - Player Area
-  - Enter Name / Show Current Player
+  - ~~Enter Name / Show Current Player~~
   - Show whose turn it is
-  - Show choice options
+  - ~~Show choice options~~
   - Visually tell when control moves to the next player
   - Show each players choice, and display winner/loser context
   - Keep track of wins and losses and show updates
@@ -13,12 +13,13 @@
 
 - Back-end
   - ~~Connect to Firebase~~
-  - Read current game state. If player1 is taken, show player2 as the login option
+  - ~~push user to connected when they choose a player?~~
+  - ~~Read current game state. If player1 is taken, show player2 as the login option~~
   - Track whose turn it is
   - Move control to active player
   - Disable player choice options when not their turn
   - Wait for choice to be made (add timer?)
-  - Once choice is made move to next player o fdetermine winner
+  - Once choice is made move to next player or determine winner
   - Send results to front end
 
 - Extra Features
