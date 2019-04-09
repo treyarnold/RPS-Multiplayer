bab0fefe5730f3b476e56b9b5077a252d4a5edcf
Trey Arnold <arnold.trey80@gmail.com>
2019-04-09 15:43:10 -0500

    complete logic restructure. currently at who won

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,7 @@ firebase.initializeApp(config);
 const DB = firebase.database();
 const connectionsRef = DB.ref("/players");
 const connectedRef = DB.ref(".info/connected");
+const gameState = DB.ref("gameState");
 
 connectedRef.on("value", snapshot => {
   if (snapshot.val()) {
@@ -48,30 +49,33 @@ const game = {
     $(playerID).append(newPlayerHeader);
   },
 
-  playerTurn: function (player) {
+  showPlayerTurn: function (player) {
     const id = `#${player}Div`;
     $(id).removeClass("waitingForTurn");
     $(id).addClass("playerTurn");
   },
 
-  waitingForTurn: function (player) {
+  showWaitingForTurn: function (player) {
     const id = `#${player}Div`;
-    $(id).addClass("playerTurn");
+    $(id).removeClass("playerTurn");
     $(id).addClass("waitingForTurn");
   },
 
   addButtonListeners: function (player) {
     $(`#${player}Rock`).on("click", () => {
-      DB.ref(`gameState/playerChoices/${player}`).set({ playerChoice: "Rock" });
-      this.switchPlayers();
+      DB.ref(`playerChoices/${player}`).set({ playerChoice: "Rock" });
+      if (this.localPlayerNumber === "1") gameState.set({ phase: "player2Turn" })
+      else gameState.set({ phase: "determineResults" })
     })
     $(`#${player}Paper`).on("click", () => {
-      DB.ref(`gameState/playerChoices/${player}`).set({ playerChoice: "Paper" });
-      this.switchPlayers();
+      DB.ref(`playerChoices/${player}`).set({ playerChoice: "Paper" });
+      if (this.localPlayerNumber === "1") gameState.set({ phase: "player2Turn" })
+      else gameState.set({ phase: "determineResults" })
     })
     $(`#${player}Scissors`).on("click", () => {
-      DB.ref(`gameState/playerChoices/${player}`).set({ playerChoice: "Scissors" });
-      this.switchPlayers();
+      DB.ref(`playerChoices/${player}`).set({ playerChoice: "Scissors" });
+      if (this.localPlayerNumber === "1") gameState.set({ phase: "player2Turn" })
+      else gameState.set({ phase: "determineResults" })
     })
   },
 
@@ -88,39 +92,47 @@ const game = {
   },
 
   startGame: function () {
-    this.playerTurn("player1");
-    this.waitingForTurn("player2");
+    this.showPlayerTurn("player1");
+    this.showWaitingForTurn("player2");
     $("#combatDiv").empty();
     if (this.localPlayerNumber === "1") {
-      this.addButtonListeners("player1");
       $("#combatDiv").append("<h2>").text("It is your Turn");
+      this.enableButtons("player1");
     } else if (this.localPlayerNumber === "2") {
-      this.disableButtons("player1");
       $("#combatDiv").append("<h2>").text("Waiting for Player 1 to choose");
-      DB.ref("gameState/playerChoices").on("value", () => this.switchPlayers());
     }
-    this.disableButtons("player2");
   },
-  
+
   switchPlayers: function () {
-    let p1 = undefined;
-    DB.ref("gameState/playerChoices/player1").once("value", snapshot => {
-      p1 = snapshot.val().playerChoice;
-    })
-    this.playerTurn("player2")
-    this.waitingForTurn("player1");
+    let player1Choice = undefined;
+    DB.ref("playerChoices/player1").once("value", snapshot => player1Choice = snapshot.val().playerChoice);
+    this.showPlayerTurn("player2")
+    this.showWaitingForTurn("player1");
     $("#combatDiv").empty();
     if (this.localPlayerNumber === "1") {
       this.disableButtons("player1");
       $("#combatDiv").append("<h2>").text("Waiting for Player 2 to Choose");
-      $("#combatDiv").append("<h2>").text(`You Chose ${p1}`);
+      $("#combatDiv").append("<h2>").text(`You Chose ${player1Choice}`);
     } else if (this.localPlayerNumber === "2") {
       $("#combatDiv").append("<h2>").text("It is your Turn");
       this.enableButtons("player2");
-      this.addButtonListeners("player2")
     }
   },
+
+  determineResults: function () {
+    DB.ref("playerChoices/player1").once("value", snapshot => {
+      this.player1Choice = snapshot.val().playerChoice;
+    })
+    DB.ref("playerChoices/player2").once("value", snapshot => {
+      this.player2Choice = snapshot.val().playerChoice;
+    })
+  }
 };
+
+game.addButtonListeners("player1");
+game.addButtonListeners("player2");
+game.disableButtons("player1");
+game.disableButtons("player2");
 
 $(document).on("click", ".player", event => {
   event.preventDefault();
@@ -134,6 +146,26 @@ $(document).on("click", ".player", event => {
   game.localPlayerNumber = playerNumber;
   DB.ref(`players/${game.localID}`).update(player);
 });
+
+gameState.on("value", snapshot => {
+  console.log(snapshot.val());
+  console.log(snapshot.val() !== null);
+  if (snapshot.val() !== null) {
+    console.log("in if");
+    const currentPhase = snapshot.val().phase;
+    switch (currentPhase) {
+      case "player1Turn":
+        game.startGame();
+        break;
+      case "player2Turn":
+        game.switchPlayers();
+        break;
+      case "determineResults":
+        game.determineResults();
+        break;
+    }
+  }
+})
 
 DB.ref("players").on("value", snapshot => {
   let player1 = false;
@@ -156,5 +188,5 @@ DB.ref("players").on("value", snapshot => {
     if (game.localPlayerNumber) game.addPlayerInput("2", true);
     else game.addPlayerInput("2", false);
   }
-  if (player1 && player2) game.startGame();
+  if (player1 && player2) gameState.set({ phase: "player1Turn" });
 });
